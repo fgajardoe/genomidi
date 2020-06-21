d38cea1134cd15ea1ffdd6c0689dad827980f688
fgajardoe <felip@maggie.localdomain>
2020-06-21 05:44:54 -0400

    Fix at utils/midi2mp3.sh

--- utils/midi2mp3.sh
+++ utils/midi2mp3.sh
@@ -1,1 +1,2 @@
-timidity $1 -Ow -o - |ffmpeg -i - -acodec libmp3lame -ab 64k $1.mp3
+ffmpeg -i $1 -acodec libmp3lame -ab 64k $1.mp3
+#timidity $1 -Ow -o - |ffmpeg -i - -acodec libmp3lame -ab 64k $1.mp3
