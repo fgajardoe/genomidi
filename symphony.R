7e3c8b0d252fa5d85c5b8c18982f05a732c036c8
fgajardoe <felip@maggie.localdomain>
2020-06-21 04:22:19 -0400

    a bit of work on the readme

--- symphony.R
+++ symphony.R
@@ -6,11 +6,11 @@ TEs.bed=read.table("data/danRer10.fa.align.recalculated.bed.order")
 TEs.gr=GRanges(seqnames=TEs.bed$V1, ranges=IRanges(start=TEs.bed$V2,end=TEs.bed$V3),strand=TEs.bed$V6,names=TEs.bed$V4,kimura=TEs.bed$V7,fixed_vol=rep(100,times=length(TEs.bed$V7)))
 
 GENEs.bed=read.table("data/refGene.bed.without-gene-name")
-GENEs.gr=GRanges(seqnames=GENEs.bed$V1, ranges=IRanges(start=GENEs.bed$V2,end=GENEs.bed$V3),strand=GENEs.bed$V6,names=GENEs.bed$V4,note=rep(69,times=length(GENEs.bed$V7)),fixed_vol=rep(127,times=length(GENEs.bed$V7)))   #vol and notes range: 0-127
+GENEs.gr=GRanges(seqnames=GENEs.bed$V1, ranges=IRanges(start=GENEs.bed$V2,end=GENEs.bed$V3),strand=GENEs.bed$V6,names=GENEs.bed$V4,note=rep(50,times=length(GENEs.bed$V7)),fixed_vol=rep(127,times=length(GENEs.bed$V7)))   #vol and notes range: 0-127
 
 
 ATACSEQ.bed=read.table("data/WT_atac.peaks.col1-6.liftover-danRer10.asterix-strand.bed")
-ATACSEQ.gr=GRanges(seqnames=ATACSEQ.bed$V1, ranges=IRanges(start=ATACSEQ.bed$V2,end=ATACSEQ.bed$V3),strand=ATACSEQ.bed$V6,names=rep("ATAC",times=length(ATACSEQ.bed$V1)),note=rep(56,times=length(ATACSEQ.bed$V1)),fixed_vol=rep(127,times=length(ATACSEQ.bed$V1)))   #vol and notes range: 0-127
+ATACSEQ.gr=GRanges(seqnames=ATACSEQ.bed$V1, ranges=IRanges(start=ATACSEQ.bed$V2,end=ATACSEQ.bed$V3),strand=ATACSEQ.bed$V6,names=rep("ATAC",times=length(ATACSEQ.bed$V1)),note=rep(57,times=length(ATACSEQ.bed$V1)),fixed_vol=rep(127,times=length(ATACSEQ.bed$V1)))   #vol and notes range: 0-127
 
 
 
@@ -21,7 +21,8 @@ T.ATAC=Track(
  Aes=list(Name="names", Note="note", Vol="fixed_vol"),
  Name="ATAC", 
  Channel=6,
- Instrument=91 # Steel drums
+ Instrument=53 # Voice Ooh
+# Instrument=19 # Organo de iglesia
 )
 
 T.GENE=Track(
@@ -39,7 +40,7 @@ T.LINE=Track(
  Aes=list(Name="names", Note="kimura", Vol="fixed_vol"),
  Name="LINE", 
  Channel=1,
- Instrument=44 # tremolo strings
+ Instrument=43 # contrabajo
 )
 
 T.SINE=Track(
@@ -47,7 +48,7 @@ T.SINE=Track(
  Aes=list(Name="names", Note="kimura", Vol="fixed_vol"),
  Name="SINE", 
  Channel=2,
- Instrument=10 # music box
+ Instrument=40 # violin
 )
 
 
@@ -70,27 +71,27 @@ T.LTR=Track(
 )
 
 # Put them all togeter in a symphony
-#S=Symphony(
-# Label="The-repetitive-order",
-# Chr="chr19",
-# Start=19490627,
-# End=20123884,
-# Tempo=100000,
-# TrackList=list(T.GENE)
-# TrackList=list(T.ATAC,T.GENE,T.LINE,T.SINE,T.DNA,T.LTR)
-#)
-
-
-
 S=Symphony(
  Label="The-repetitive-order",
- Chr="chr23",
- Start=35300000,
- End=36900000,
+ Chr="chr19",
+ Start=19490627,
+# End=20123884,
+ End=30123884,
  Tempo=100000,
 # TrackList=list(T.GENE)
  TrackList=list(T.ATAC,T.GENE,T.LINE,T.SINE,T.DNA,T.LTR)
 )
+
+
+
+#S=Symphony(
+# Label="The-repetitive-order",
+# Chr="chr23",
+# Start=35300000,
+# End=36900000,
+# Tempo=100000,
+# TrackList=list(T.ATAC,T.GENE,T.LINE,T.SINE,T.DNA,T.LTR)
+#)
 
 # And compose them by bringing all elements comprending the selected region
 composeSymphony(S)
